f861c9b0fc9682e3f98072672b6b7b00bf733de4
eediallo <elhhabdouldiallo@gmail.com>
2025-07-06 15:28:09 +0100

    Add DeskGrid component with styling

---
+++ web/src/components/DeskGrid.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+
+import Desk from "./Desk";
+import "./DeskGrid.css";
+
+const DeskGrid = ({ desks, bookings, onDeskClick, currentUserId }) => {
+	const findBookingForDesk = (deskId) => {
+		return bookings.find((b) => b.desk_id === deskId);
+	};
+
+	return (
+		<div className="desk-grid">
+			{desks.map((desk) => (
+				<Desk
+					key={desk.id}
+					desk={desk}
+					booking={findBookingForDesk(desk.id)}
+					onClick={onDeskClick}
+					currentUserId={currentUserId}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default DeskGrid;
+
+DeskGrid.propTypes = {
+	desks: PropTypes.array.isRequired,
+	bookings: PropTypes.array.isRequired,
+	onDeskClick: PropTypes.func.isRequired,
+	currentUserId: PropTypes.number,
+};

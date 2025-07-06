f4416d6f736f83bd868548a42fda1ed509405daf
eediallo <elhhabdouldiallo@gmail.com>
2025-07-06 22:41:29 +0100

    Wrap App component with AppProvider for state management

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import App from "./App.jsx";
+import { AppProvider } from "./context/AppContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<AppProvider>
+				<App />
+			</AppProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 );

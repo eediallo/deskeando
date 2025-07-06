2f540bde137cef22afaedb77f1c90c03010bd16e
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-07-06 10:18:40 +0100

    comment out messages module

--- api/messages/messageService.js
+++ api/messages/messageService.js
@@ -1,6 +1,6 @@
-import * as repository from "./messageRepository.js";
+/* import * as repository from "./messageRepository.js";
 
 export async function getMessage() {
 	const [first] = await repository.getAll();
 	return first.content;
-}
+} */

4d64c5a4780e545e0f8204bc1d8c3807d54db62d
Anna Fedyna <annafedyna03@gmail.com>
2025-07-07 18:53:45 +0100

    feac: GET all bookings and DELETE booking by id

--- api/modules/bookings/bookingRouter.js
+++ api/modules/bookings/bookingRouter.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 
-import { handleCreateBooking } from "./bookingService.js";
+import {
+	handleCreateBooking,
+	getAllBookings,
+	deleteBookingById,
+	getBookingById,
+} from "./bookingService.js";
 
 const router = Router();
+
+router.get("/", (_, res) => {
+	res.send(getAllBookings());
+});
 
 // Not RESTful - should be POST /api/v1/bookings - might consider changing it
 router.post("/create_booking", (req, res) => {
@@ -14,6 +23,25 @@ router.post("/create_booking", (req, res) => {
 
 	const booking = handleCreateBooking({ user_id, desk_id });
 	res.status(201).json(booking);
+});
+
+router.delete("/delete_booking/:id", (req, res) => {
+	const bookingId = Number(req.params.id);
+	const { user_id } = req.body;
+	const booking = getBookingById(bookingId);
+
+	if (!booking) {
+		return res.status(404).json({ error: "Booking not found!" });
+	}
+
+	if (user_id && booking.user_id !== user_id) {
+		return res
+			.status(403)
+			.json({ error: "Unauthorized to delete this booking" });
+	}
+
+	deleteBookingById(bookingId);
+	res.status(204).send();
 });
 
 export default router;

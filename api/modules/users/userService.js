9d0ebe127b43894f1f22624b941e6d35f91448fa
Anna Fedyna <annafedyna03@gmail.com>
2025-07-07 13:57:51 +0100

    feac: get users

--- api/modules/users/userService.js
+++ api/modules/users/userService.js
@@ -1,5 +1,9 @@
-import { registerUser } from "./userRepository.js";
+import { registerUser, getAll } from "./userRepository.js";
 
 export function handleRegisterUser({ firstName, lastName }) {
 	return registerUser({ firstName, lastName });
+}
+
+export function getUsers() {
+	return getAll();
 }

acac9e46306c783e78bf55fd15cc09be634ee39c
eediallo <elhhabdouldiallo@gmail.com>
2025-07-14 21:39:02 +0100

    fix: validate email input in registerUser function

--- api/modules/users/userRepository.js
+++ api/modules/users/userRepository.js
@@ -6,11 +6,14 @@ export async function getAll() {
 }
 
 export async function registerUser({ firstName, lastName, email, password }) {
+	if (!email || typeof email !== "string" || !email.trim()) {
+		throw new Error("Email is required");
+	}
 	const query = `
-		   INSERT INTO "user" (first_name, last_name, email, password)
-		   VALUES ($1, $2, $3, $4)
-		   RETURNING *;
-	   `;
+	   INSERT INTO "user" (first_name, last_name, email, password)
+	   VALUES ($1, $2, $3, $4)
+	   RETURNING *;
+   `;
 
 	const values = [firstName, lastName, email, password];
 	const { rows } = await db.query(query, values);

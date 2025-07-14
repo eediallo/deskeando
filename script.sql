45fc02711b6029407e5c707a5a3f51b95af02834
eediallo <elhhabdouldiallo@gmail.com>
2025-07-14 21:35:02 +0100

    fix: add email and password fields to user table and update insert statements

--- script.sql
+++ script.sql
@@ -11,7 +11,9 @@ CREATE TABLE "desk"(
 CREATE TABLE "user" (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     "first_name" text,
-    "last_name" text
+    "last_name" text,
+    "email" text not null UNIQUE,
+    "password" VARCHAR(255)
 );
 
 CREATE TABLE "booking"(
@@ -30,12 +32,12 @@ INSERT INTO "desk" (id, name) VALUES
 ('db4f01e4-9d64-4732-a099-9664db206f08','Desk 4'),
 ('5b3d6606-4dd1-4e01-92fb-889303c5939a','Desk 5');
 
-INSERT INTO "user" (id, first_name, last_name) VALUES
-('26136694-7c90-41c3-9787-b7f0bd776a23', 'Alice', 'Smith'),
-('c6dbfcba-a714-4f26-a658-f6292ca7586e','Bob', 'Johnson'),
-('fbd8d766-73b9-4cd0-b11e-8fb507ca0d53','Charlie', 'Lee'),
-('82f7e975-c803-4fb8-be96-4f7c9bfa6a0c','Diana', 'Brown'),
-('35abf422-88d4-4de4-aa02-7294e8ac796e','Ethan', 'Wilson');
+INSERT INTO "user" (id, first_name, last_name, email, password) VALUES
+('26136694-7c90-41c3-9787-b7f0bd776a23', 'Alice', 'Smith', 'alice@gmail.com', 'sosecret'),
+('c6dbfcba-a714-4f26-a658-f6292ca7586e','Bob', 'Johnson', 'bob@gmail.cl', 'sosecret'),
+('fbd8d766-73b9-4cd0-b11e-8fb507ca0d53','Charlie', 'Lee', 'charlie@gmail.com', 'sosecret'),
+('82f7e975-c803-4fb8-be96-4f7c9bfa6a0c','Diana', 'Brown', 'diana@gmail.com', 'sosecret'),
+('35abf422-88d4-4de4-aa02-7294e8ac796e','Ethan', 'Wilson', 'ethan@gmail.com', 'sosecret');
 
 INSERT INTO "booking" (desk_id, user_id, from_date, to_date) VALUES
 ('b142a09d-76f7-4140-a401-52a7bc5f22c5', '26136694-7c90-41c3-9787-b7f0bd776a23', '2025-07-01', '2025-07-01'),

5d0ad73122d92dde4d30bec846ef01da696a395c
Anna Fedyna <annafedyna03@gmail.com>
2025-07-16 00:34:54 +0100

    feat: add input validation checks for user registration

--- api/modules/users/authRouter.js
+++ api/modules/users/authRouter.js
@@ -9,10 +9,38 @@ const authRouter = Router();
 authRouter.post("/register", async (req, res) => {
 	const { firstName, lastName, email, password } = req.body;
 
-	if (!firstName || !lastName || !email || !password) {
+	if (
+		!firstName.trim() ||
+		!lastName.trim() ||
+		!email.trim() ||
+		!password.trim()
+	) {
 		return res
 			.status(400)
 			.json({ error: "Missing firstName, lastName, email, or password" });
+	}
+
+	const nameIsValid = (name) => /^[a-zA-Z\s.'-]+$/.test(name);
+	if (!nameIsValid(firstName) || !nameIsValid(lastName)) {
+		return res.status(400).json({ error: "Invalid characters in name." });
+	}
+
+	const validateEmail = (email) => {
+		return String(email)
+			.toLowerCase()
+			.match(
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			);
+	};
+
+	if (!validateEmail(email)) {
+		return res.status(400).json({ error: "Invalid email address." });
+	}
+
+	if (password.length < 8) {
+		return res
+			.status(400)
+			.json({ error: "Password must be at least 8 characters long." });
 	}
 
 	try {

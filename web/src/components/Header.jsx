dfc995fc69768375af0e8bea8d028330fe5b2f15
eediallo <elhhabdouldiallo@gmail.com>
2025-07-15 22:27:38 +0100

    feat: update Header component to include authentication-based navigation links

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,12 +1,26 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+
+import { useAppContext } from "../context/useAppContext";
 import "./Header.css";
 
 const Header = () => {
+	const { isAuthenticated } = useAppContext();
 	return (
 		<header className="app-header">
+			<div className="logo">CompanyLogo</div>
 			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/calendar">Calendar</Link>
+				{!isAuthenticated && (
+					<>
+						<Link to="/login">Login</Link>
+						<Link to="/register">Sign Up</Link>
+					</>
+				)}
+				{isAuthenticated && (
+					<>
+						<Link to="/">Home</Link>
+						<Link to="/calendar">Calendar</Link>
+					</>
+				)}
 			</nav>
 		</header>
 	);

af4cfdd8a868197ac7f78df21870fd7368d56829
eediallo <elhhabdouldiallo@gmail.com>
2025-07-19 14:51:08 +0100

    style: refactor SQL queries in userRepository.js to use sql tagged template for improved readability

--- api/modules/users/userRepository.js
+++ api/modules/users/userRepository.js
@@ -1,34 +1,57 @@
 import bcryptjs from "bcryptjs";
 
 import db from "../../db.js";
+import { sql } from "../../utils/database.js";
 
 export async function getAll() {
-	const { rows } = await db.query('SELECT * FROM "user";');
+	const { rows } = await db.query(
+		sql`
+		SELECT 
+			id, 
+			first_name, 
+			last_name, 
+			email 
+		FROM "user";`,
+	);
 	return rows;
 }
 
 export async function registerUser({ firstName, lastName, email, password }) {
-	const checkQuery = `SELECT id FROM "user" WHERE email = $1`;
-	const checkResult = await db.query(checkQuery, [email]);
+	const checkResult = await db.query(
+		sql`
+		SELECT 
+			id
+		FROM "user" 
+		WHERE email = $1
+	`,
+		[email],
+	);
 	if (checkResult.rows.length > 0) {
 		throw new Error("Email already in use.");
 	}
 
 	const hashedPassword = await bcryptjs.hash(password, 10);
 
-	const query = `
-	   INSERT INTO "user" (first_name, last_name, email, password)
+	const values = [firstName, lastName, email, hashedPassword];
+	const { rows } = await db.query(
+		sql`
+	   INSERT 
+	   	INTO "user" (first_name, last_name, email, password)
 	   VALUES ($1, $2, $3, $4)
 	   RETURNING *;
-   `;
-
-	const values = [firstName, lastName, email, hashedPassword];
-	const { rows } = await db.query(query, values);
+   `,
+		values,
+	);
 	return rows[0];
 }
 
 export async function findUserByEmail(email) {
-	const query = 'SELECT * FROM "user" WHERE email = $1';
-	const { rows } = await db.query(query, [email]);
+	const { rows } = await db.query(
+		sql`
+		SELECT * FROM "user"
+			WHERE email = $1
+	`,
+		[email],
+	);
 	return rows[0];
 }

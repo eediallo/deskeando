8d7f96786bab8e6301513fdd5faf3c6cf0ebfbc4
Anna Fedyna <annafedyna03@gmail.com>
2025-07-29 16:36:14 +0100

    refactor: remove unused fixture

--- api/fixtures/bookings.js
+++
@@ -1,9 +0,0 @@
-export let bookings = [
-	{
-		id: "123",
-		deskId: "abc",
-		userId: "2",
-		fromDate: "2025-07-05T13:00:00",
-		toDate: "2025-07-05T19:00:00",
-	},
-];
--- api/fixtures/users.js
+++
@@ -1,4 +0,0 @@
-export const users = [
-	{ id: "1", firstName: "Lucas", lastName: "Smith" },
-	{ id: "2", firstName: "Edlahdj", lastName: "Jones" },
-];
